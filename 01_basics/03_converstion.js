0d42eda6cafe963174e03874da4f64502b61f53e
nirajkumar2001 <113297572+nirajkumar2001@users.noreply.github.com>
2024-07-17 01:29:26 +0000

    opretaions

--- 01_basics/03_converstion.js
+++ 01_basics/03_converstion.js
@@ -1,24 +1,61 @@
-let score="33";// if i write this in the double quotes then it become the string
-console.log(typeof score);//  for knowing the type of the declared variable
-let valueNumber=Number(score);// if the string is not fully type of number then it return nan
-console.log(typeof valueNumber);
-console.log( valueNumber)
-/* when i convert the 33 to number it gets converted but if i try to change the string it gives us nan*/
-//333=333
-//niraj=nan
-//true=1,false=0 after converting it into the number
-let isLoggedIn=1
-let booleanloggedIn=Boolean(isLoggedIn)
-console.log( typeof booleanloggedIn)
-console.log( booleanloggedIn)
-/*
-1=true,0=false
-""=false
-"Niraj"=true
-*/
-let number=333
-let strignNumber=String( number)
-console.log( typeof strignNumber)
-console.log(strignNumber)
+// let score="33";// if i write this in the double quotes then it become the string
+// console.log(typeof score);//  for knowing the type of the declared variable
+// let valueNumber=Number(score);// if the string is not fully type of number then it return nan
+// console.log(typeof valueNumber);
+// console.log( valueNumber)
+// /* when i convert the 33 to number it gets converted but if i try to change the string it gives us nan*/
+// //333=333
+// //niraj=nan
+// //true=1,false=0 after converting it into the number
+// let isLoggedIn=1
+// let booleanloggedIn=Boolean(isLoggedIn)
+// console.log( typeof booleanloggedIn)
+// console.log( booleanloggedIn)
+// /*
+// 1=true,0=false
+// ""=false
+// "Niraj"=true
+// */
+// let number=333
+// let strignNumber=String( number)
+// console.log( typeof strignNumber)
+// console.log(strignNumber)
+
+/////****************** Operations *******************///////
+// let value=4;
+// let negvalue=-value;
+// console.log(negvalue)
+// console.log(2+2)
+// console.log( 2-2)
+// console.log( 2*2)
+// console.log( 2**2)// it gives us the power
+// console.log( 2/2)//Quotient
+// console.log( 2%2)// remainder
+
+// let str1="Niraj"
+// let str2=" Kumar"
+// let str3=str1+str2
+// console.log(str3)
+// console.log( 1+20)
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log( 2+"1")
+console.log( 2+2+"3") // which part is in starting it act as that type
+console.log(+true)
+console.log(+"")
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
 
 
