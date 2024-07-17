914478d86eb119b39ec51ccd3905eeb46c0cfffa
nirajkumar2001 <113297572+nirajkumar2001@users.noreply.github.com>
2024-07-17 01:11:37 +0000

    conversion of datatype

---
+++ 01_basics/03_converstion.js
@@ -0,0 +1,24 @@
+let score="33";// if i write this in the double quotes then it become the string
+console.log(typeof score);//  for knowing the type of the declared variable
+let valueNumber=Number(score);// if the string is not fully type of number then it return nan
+console.log(typeof valueNumber);
+console.log( valueNumber)
+/* when i convert the 33 to number it gets converted but if i try to change the string it gives us nan*/
+//333=333
+//niraj=nan
+//true=1,false=0 after converting it into the number
+let isLoggedIn=1
+let booleanloggedIn=Boolean(isLoggedIn)
+console.log( typeof booleanloggedIn)
+console.log( booleanloggedIn)
+/*
+1=true,0=false
+""=false
+"Niraj"=true
+*/
+let number=333
+let strignNumber=String( number)
+console.log( typeof strignNumber)
+console.log(strignNumber)
+
+
