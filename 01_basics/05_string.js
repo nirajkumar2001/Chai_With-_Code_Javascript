255a159c219ddb8656e368159a62828b33cc9c9d
nirajkumar2001 <113297572+nirajkumar2001@users.noreply.github.com>
2024-07-18 01:56:32 +0000

    string method

---
+++ 01_basics/05_string.js
@@ -0,0 +1,31 @@
+//  const name="hitesh"
+//  const repocount=50
+//  //console.log(name+repocount+"value");
+//  console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+  const gamename=new String('nirajkumar')
+//  console.log(gamename[0]);
+//  console.log(gamename.__proto__);
+//  console.log( gamename.length)
+//  console.log(gamename.toUpperCase());// the orignal value has not changed
+//  console.log(gamename.charAt(5))
+//  console.log(gamename.indexOf('j'))
+const newString=gamename.substring(0,4)
+console.log(newString)
+const anotherstring=gamename.slice(-9,4)
+console.log(anotherstring)
+const newStringone="    nirajkumar  "
+console.log(newStringone)
+console.log(newStringone.trim())// it works only on the whitespace and there are two method also trimstart ans trimend
+const url="https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','_'))
+console.log(url)
+console.log(gamename.split('_') )
+
+// learn about the string method
+
+
+
+
+
+
+ 
