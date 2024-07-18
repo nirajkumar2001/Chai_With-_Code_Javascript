31b0679a93b7adec669a47887d6b25acc75b38e0
nirajkumar2001 <113297572+nirajkumar2001@users.noreply.github.com>
2024-07-18 00:21:14 +0000

    comaparison of different data types

---
+++ 01_basics/comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+//console.log(2!=1)// same as the other laguage or mathematical
+// /console.log( "2">1)
+//console.log("02">1)// always try to compare the same type of datatype
+// console.log( null >0)
+// console.log(null==0)
+// console.log(null>=0)
+
+/* the reason is that an equality check == and comparisons > < >= <= work differently, comparison convert null to a number treating it as 0.that's why (3) null>=0 is true and(1) null >0 is false*/
+//console.log( undefined>0) // it always give false
+// === here it always check the data check that's why we called it strict check
+console.log( "2"===4)
+
+
+
+
+
+
