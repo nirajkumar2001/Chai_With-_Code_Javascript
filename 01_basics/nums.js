f80792736ebe47c9d61ddc08af7d7e91fbc8a024
nirajkumar2001 <113297572+nirajkumar2001@users.noreply.github.com>
2024-07-25 02:29:40 +0000

    mathnum

---
+++ 01_basics/nums.js
@@ -0,0 +1,31 @@
+const score =4400;
+const balance = new Number (900);
+// console.log(balance);// when we defined in new keyword then it tell us that it string or number
+// console.log(score);
+// console.log(balance.toString());
+// console.log( typeof balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // always use this type when we make ecommerc website
+const otherNumber=1123.4466
+// console.log(otherNumber.toPrecision(3))
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'))// we can check on the mdn documantation
+// /////+++++++++++++++++++++++++++++++++++++++////
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(54));
+// console.log(Math.min(4,5,6,4,3,9));
+console.log( Math.random());
+// math.random value always lies in 0 to 1;
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+ const min=10
+ const max=20
+ //when it start with 0 that' why we are multipy with 10 and add 1 in this 
+ console.log(Math.floor(Math.random()*(max-min+1))+min)
+ 
+
+
