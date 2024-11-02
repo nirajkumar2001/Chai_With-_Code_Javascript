c2f4893d6ca774d61b53afb98a4e9e99d6260064
nirajkumar2001 <113297572+nirajkumar2001@users.noreply.github.com>
2024-11-02 22:19:13 +0000

    array

--- 01_basics/array.js
+++ 01_basics/array.js
@@ -1,5 +1,14 @@
-let names=[ 'love', 'rahul', 'niraj', 'babu'];
- console.log( names[1]);
- console.log( names[4])
- names[1]='jo man kre dal do '// ye jo hai na value ko array me override kr dega
-console.log( names[1])// if we want to assign the same varibale with the let keyword then the ------- cut the variable names
+// let names=[ 'love', 'rahul', 'niraj', 'babu'];
+//  console.log( names[1]);
+//  console.log( names[4])
+//  names[1]='jo man kre dal do '// ye jo hai na value ko array me override kr dega
+// console.log( names[1])// if we want to assign the same varibale with the let keyword then the ------- cut the variable names
+ for (let i=0; i<5; i++){
+    console.log( i);
+ }
+  let num=10;
+  while(num>=0){
+    console.log(num);
+    num--;
+  }
+  
